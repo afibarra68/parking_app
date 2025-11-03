72ede4296d2c454318c8f8db49aeeb37d7a8ea4f
andresid <andres.ih.developer@gmail.com>
2025-11-03 15:05:11 -0500

    stable version integral de seguridad de usuarios!
    TODO: pulir llaves unicas por bloque, integrar comandos de limpieza, integrar programacion reactiva

--- src/main/java/com/webstore/usersMs/config/SecurityConfig.java
+++ src/main/java/com/webstore/usersMs/config/SecurityConfig.java
@@ -2,17 +2,12 @@ package com.webstore.usersMs.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
