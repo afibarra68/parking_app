70171187ab0bd5f3d2367384a89db33b1be26922
andresid <andres.ih.developer@gmail.com>
2025-11-29 23:47:48 -0500

    feat: ignorar campo paymentDay en creación y actualización de clientes
    - Agregado @Mapping para ignorar paymentDay en fromDto() y merge()
    - El campo sigue siendo visible en consultas (toDto())
    - Actualizado CHANGELOG.md con notas de release
    
    Cambios técnicos:
    - ClientMapper.java: Agregadas anotaciones @Mapping(target = "paymentDay", ignore = true)
    - El campo paymentDay ya no se procesa en operaciones de creación/actualización
    
    Estado: Listo para merge a main

--- src/main/java/com/webstore/usersMs/mappers/ClientMapper.java
+++ src/main/java/com/webstore/usersMs/mappers/ClientMapper.java
@@ -3,7 +3,9 @@ package com.webstore.usersMs.mappers;
 import static org.mapstruct.ReportingPolicy.IGNORE;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.springframework.data.domain.Page;
 
 import com.webstore.usersMs.dtos.DClient;
 import com.webstore.usersMs.entities.Client;
@@ -13,15 +15,21 @@ import java.util.stream.Collectors;
 @Mapper(unmappedTargetPolicy = IGNORE)
 public interface ClientMapper {
 
+    @Mapping(target = "paymentDay", ignore = true)
     Client fromDto(DClient dto);
 
     DClient toDto(Client entity);
 
+    @Mapping(target = "paymentDay", ignore = true)
     Client merge(DClient dto, @MappingTarget Client client);
 
     default List<DClient> toList(List<Client> list){
         return list.stream().map(this::toDto).collect(Collectors.toList());
     }
 
+    default Page<DClient> toPage(Page<Client> page) {
+        return page.map(this::toDto);
+    }
+  
 }
 
